27a18df52852bbe67d3ad8753fa8a94ee8ae05ec
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-05-09 11:12:49 +0200

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
 app.get('/', (req, res) => {
     res.send('Hello World! thomas')
 })
